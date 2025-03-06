c7701dc3c14822b4348967b5ab59b46702905436
“GeorgeGao128” <2660674188@qq.com>
2025-03-06 18:12:38 +0800

    第三周作业

---
+++ assignment3/Week3/Q1/Program.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Reflection.Metadata;
+
+namespace Week3;
+using System.Collections.Generic;
+class Program
+{
+    public class Shape
+    {
+        public Shape(int le,int he)
+        {
+            length = le;
+            height = he;
+        }
+        protected int length{get;set;}
+        protected int height{get;set;}
+        public int GetArea() => (length * height);
+
+        public bool IsLegal()
+        {
+            if (length < 1 || length > 100) return false;
+            if (height < 1 || height > 100) return false;
+            return true;
+        }
+    }
+
+    public class Triangle : Shape
+    {
+        public Triangle(int a, int b) : base(a, b)
+        {
+            length = a;
+            height = b;
+        }
+        new public int GetArea() => (length * height) / 2;
+    }
+    
+    public class Rectangle : Shape
+    {
+        public Rectangle(int a, int b) : base(a, b)
+        {
+            length = a;
+            height = b;
+        }
+        
+    }
+    
+    public class Square : Shape
+    {
+        public Square(int a, int b) : base(a, b)
+        {
+            length = a;
+            height = b;
+        }
+        public new bool IsLegal()
+        {
+            if (length!=height) return false;
+            if (length < 1 || length > 100) return false;
+            if (height < 1 || height > 100) return false;
+            return true;
+        }
+    }
+    private static void Main()
+    {
+        Console.WriteLine("请输入你要创建的形状\n如：Square, Triangle, Rectangle");
+        string shape = Console.ReadLine();
+        Console.WriteLine("请输入该形状的长和高,用空格分开");
+        string [ ] inputs = Console . ReadLine ( ) . Split ( ) ;
+        int leng = int . Parse ( inputs [ 0 ] ) ;
+        int heig = int . Parse ( inputs [ 1 ] ) ;
+        switch (shape)
+        {
+            case "Square":
+                Square s= new Square(leng,heig);
+                if(s.IsLegal())
+                    Console.WriteLine($"面积为：{s.GetArea()}");
+                else
+                {
+                    Console.WriteLine("输入的数值不合法");
+                    return;
+                }
+                break;
+            case "Rectangle":
+                Rectangle r= new Rectangle(leng,heig);
+                if(r.IsLegal())
+                    Console.WriteLine($"面积为：{r.GetArea()}");
+                else
+                {
+                    Console.WriteLine("输入的数值不合法");
+                    return;
+                }
+                break;
+            case "Triangle":
+                Triangle t= new Triangle(leng,heig);
+                if(t.IsLegal())
+                    Console.WriteLine($"面积为：{t.GetArea()}");
+                else
+                {
+                    Console.WriteLine("输入的数值不合法");
+                    return;
+                }
+                break;
+        }
+        
+        
+        
+        
+    }
+}
+
---
+++ assignment3/Week3/Q2/Program.cs
@@ -0,0 +1,111 @@
+﻿using System;
+namespace Week3;
+using System.Collections.Generic;
+
+
+class Program
+{
+    // 接口定义
+    public interface IShape
+    {
+        int GetArea();
+        string GetShapeName();
+    }
+
+// 具体类实现
+    public class Triangle : IShape
+    {
+        public string ShapeName="Triangle";
+        public string GetShapeName(){return ShapeName;}
+        int area = 0;
+        public Triangle()
+        {
+            Random random = new Random();
+            int num = random.Next(1, 100);
+            area = num;
+        }
+        public int GetArea()
+        { 
+            return area; 
+        }
+    }
+    public class Circle : IShape
+    {
+        public string ShapeName = "Circle";
+        public string GetShapeName(){return ShapeName;}
+        int area = 0;
+        public Circle()
+        {
+            Random random = new Random();
+            int num = random.Next(1, 100);
+            area = num;
+        }
+        public int GetArea() // 必须实现具体逻辑
+        { 
+            return area; 
+        }
+    }
+    public class Rectangle : IShape
+    {
+        public string ShapeName = "Rectangle";
+        public string GetShapeName(){return ShapeName;}
+        int area = 0;
+        public Rectangle()
+        {
+            Random random = new Random();
+            int num = random.Next(1, 100);
+            area = num;
+        }
+        public int GetArea() // 必须实现具体逻辑
+        { 
+            return area; 
+        }
+    }
+    public class Square : IShape
+    {
+        public string ShapeName = "Square";
+        public string GetShapeName(){return ShapeName;}
+        int area = 0;
+        public Square()
+        {
+            Random random = new Random();
+            int num = random.Next(1, 100);
+            area = num;
+        }
+        public int GetArea() // 必须实现具体逻辑
+        { 
+            return area; 
+        }
+    }
+
+    public class ShapeFactory
+    {
+        public IShape CreateShape()
+        {
+            Random rand = new Random();
+            int num = rand.Next ( ) ;
+            num %= 4;
+            return num switch
+            {
+                1 => new Triangle(),
+                2 => new Circle(),
+                3 => new Rectangle(),
+                0 => new Square(),
+                _ => throw new NotImplementedException()
+            };
+        }
+    }
+    private static void Main()
+    {
+        ShapeFactory factory = new ShapeFactory ( ) ;
+        int SumOfArea=0;
+        for (int i = 0; i < 10; i++)
+        {
+            IShape shape = factory . CreateShape () ;
+            SumOfArea=SumOfArea + shape.GetArea();
+            string str=shape.GetShapeName();
+            Console.WriteLine ($"第{i+1}个创建的图形是{str},他的面积是{shape.GetArea()}");
+        }
+        Console.WriteLine($"这十个对象的面积之和是{SumOfArea}");
+    }
+}
